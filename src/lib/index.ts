e9fb78bf4e774cfd22aafc17716b45221663650b
MarcelRyan <marcel.ryan2004@gmail.com>
2024-03-20 18:08:21 +0700

    feat: setting up webgl

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+// import createShader from "./shader";
+// import createProgram from "./program";
+
+// /* Initialize WebGL */
+
+// const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+// const gl = canvas.getContext("webgl");
+
+// const vertexShaderElement = document.getElementById()
---
+++ src/lib/shader.ts
@@ -0,0 +1,23 @@
+function createShader(
+    gl : WebGLRenderingContext,
+    type : number,
+    source : string
+) {
+    const shader = gl.createShader(type);
+    if (!shader) {
+        console.error('Failed to create shader');
+        return null;
+    }
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    } else {
+        console.error('Failed to compile shader:', gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader); // Clean up the shader object
+        return null;
+    }
+}
+
+export default createShader;
