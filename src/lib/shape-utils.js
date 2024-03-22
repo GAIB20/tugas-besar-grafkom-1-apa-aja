d4b20455e0b31c9f3ebe9281710ed3221531ecde
mikeleo03 <leonmichael463@gmail.com>
2024-03-22 23:27:51 +0700

    feat : initialize slider and reset function parameters

--- src/lib/shape-utils.js
+++ src/lib/shape-utils.js
@@ -2,32 +2,54 @@ const canvas = document.querySelector("#ini-canvas");
 const CANVAS_WIDTH = canvas.getAttribute("width");
 const CANVAS_HEIGHT = canvas.getAttribute("height");
 
+// Function to reset all the parameter of user input to the canvas
+function resetParams() {
+    // Get the x value and then reset the min and max value to the canvas size
+    const xValue = document.getElementById("x");
+    xValue.setAttribute("min", 0);
+    xValue.setAttribute("max", CANVAS_WIDTH);
+    xValue.value = CANVAS_WIDTH / 2;
+    
+    // Get the y value and then reset the min and max value to the canvas size
+    const yValue = document.getElementById("y");
+    yValue.setAttribute("min", 0);
+    yValue.setAttribute("max", CANVAS_HEIGHT);
+    yValue.value = CANVAS_HEIGHT / 2;
+
+    // Reset the other parameters to default values
+    document.getElementById("angle").value = 0;
+    document.getElementById("scale").value = 1;
+    document.getElementById("transformX").value = 1;
+    document.getElementById("transformY").value = 1;
+}
+
 // Translate x pixel to coordinate between [-1, 1]
 function translateXPixel(x) {
     let half = CANVAS_WIDTH / 2;
     return (x - half) / half;
 }
 
-/* Translate y pixel to coordinate between [-1, 1]
-The return is negated because to make the coordinate from 0 to half positive and half to end negative */
+// Translate y pixel to coordinate between [-1, 1]
+// The return is negated because to make the coordinate from 0 to half positive and half to end negative
 function translateYPixel(y) {
     let half = CANVAS_HEIGHT / 2;
     return -(y - half) / half;
 }
 
-/* Translate x coordinate to pixel */
+// Translate x coordinate to pixel
 function translateXCoordinate(x) {
     let half = CANVAS_WIDTH / 2;
     return x * half + half;
 }
 
-/* Translate y coordinate to pixel */
+// Translate y coordinate to pixel
 function translateYCoordinate(y) {
     let half = CANVAS_HEIGHT / 2;
     return -y * half + half;
 }
 
-// += 6 karena tiap 1 titik ngandung 6 elemen (x, y, r, g, b, a)
+// Convert the coordinate to pixel
+// Every point contains 6 elements (x, y, r, g, b, a)
 function coordinateToPixel(vertices) {
     for (let i = 0; i < vertices.length; i += 6) {
         vertices[i] = translateXCoordinate(vertices[i]);
@@ -37,55 +59,55 @@ function coordinateToPixel(vertices) {
     return vertices;
 }
 
-// += 6 karena tiap 1 titik ngandung 6 elemen (x, y, r, g, b, a)
+// Convert back pixal to coordinate
 function pixelToCoordinate(vertices) {
-    for (let i = 0; i < vertices.length; i+=6) {
+    for (let i = 0; i < vertices.length; i+= 6) {
         vertices[i] = translateXPixel(vertices[i]);
-        vertices[i+1] = translateYPixel(vertices[i+1]);
+        vertices[i + 1] = translateYPixel(vertices[i + 1]);
     }
 
     return vertices;
 }
 
-// Ambil center dari shapenya
+// Get the center of the shape
 function getCenter(vertices) {
     totalX = 0;
     totalY = 0;
     n = 0;
-    for (let i = 0; i < vertices.length; i+=6) {
+    for (let i = 0; i < vertices.length; i += 6) {
         totalX += vertices[i];
-        totalY += vertices[i+1];
+        totalY += vertices[i + 1];
         n += 1;
     }
 
     return [totalX/n, totalY/n];
 }
 
-// Ini fungsi translasi buat shape
+// Doing shape translation
 function translation(vertices, dx, dy) {
-    for (let i = 0; i < vertices.length; i+=6) {
+    for (let i = 0; i < vertices.length; i += 6) {
         vertices[i] += translateXCoordinate(dx);
-        vertices[i+1] += translateYCoordinate(dy);
+        vertices[i + 1] += translateYCoordinate(dy);
     }
 
     return vertices
 }
 
-// Ini fungsi buat scaling shape
+// Doing shape scalling
 function scale(vertices, factor) {
     center = getCenter(vertices);
-    for (let i = 0; i < vertices.length; i+=6) {
+    for (let i = 0; i < vertices.length; i += 6) {
         vertices[i] = (vertices[i] - center[0]) * factor + center[0];
-        vertices[i+1] = (vertices[i+1] - center[1]) * factor + center[1];
+        vertices[i + 1] = (vertices[i + 1] - center[1]) * factor + center[1];
     }
 
     return vertices;    
 }
 
-// Ini fungsi buat ubah nilai x tiap titik shapenya
+// Change the x value of every points on that shape
 function transformX(vertices, value) {
     center = getCenter(vertices);
-    for (let i = 0; i < vertices.length; i+=6) {
+    for (let i = 0; i < vertices.length; i += 6) {
         vertices[i] = (vertices[i] - center[0]) * value + center[0];
     }
     
@@ -95,7 +117,7 @@ function transformX(vertices, value) {
 // Ini fungsi buat ubah nilai y tiap titik shapenya
 function transformY(vertices, value) {
     center = getCenter(vertices);
-    for (let i = 0; i < vertices.length; i+=6) {
+    for (let i = 0; i < vertices.length; i += 6) {
         vertices[i + 1] = (vertices[i + 1] - center[1]) * value + center[1];
     }
 
